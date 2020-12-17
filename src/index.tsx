71f1f45078538db3484baab551516b4d4d1eb646
chinsun9 <40482203+chinsun9@users.noreply.github.com>
2020-12-17 11:04:27 +0900

    setup redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
+import rootReducer from './modules';
 import reportWebVitals from './reportWebVitals';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
