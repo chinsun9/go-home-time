42c4d8ef2ae84b1094742c31e06fce41cfcb34a4
chinsun9 <40482203+chinsun9@users.noreply.github.com>
2020-12-18 16:30:25 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+
+          <Route exact path="/">
+            <Home isDefault />
+          </Route>
+
+          <Route path="/:input">
+            <Home />
+          </Route>
+
+          <Route path="*">
+            <p>im not found</p>
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
