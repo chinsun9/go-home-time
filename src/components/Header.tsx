66b71bee8ecf8f6dfe9f949a83b6dd978d02516e
chinsun9 <40482203+chinsun9@users.noreply.github.com>
2020-12-21 11:19:32 +0900

    Fix Header Link path
    about 페이지에 갔다가 다시 홈으로 돌아오면 url params이 없어지는 문제해결

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
 import './Header.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useTime from '../hooks/useMyState';
 
 function Header() {
+  const { time } = useTime();
+
   return (
     <div className="header">
-      <Link to="/">
+      <Link to={`/${time}`}>
         <h3>go home time</h3>
       </Link>
     </div>
